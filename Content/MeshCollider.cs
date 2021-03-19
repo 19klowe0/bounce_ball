e32a068cbc4f642a2ef8842768e875260ea9e657
19klowe0 <kiralowe98@gmail.com>
2021-03-19 14:23:09 -0400

    MeshCollider

---
+++ Content/MeshCollider.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace bounce_Ball.Content
+{
+    class MeshCollider
+    {
+        List<Vector3> verts;
+        List<Vector3> norms;
+
+        public MeshCollider(Model model, Matrix world)
+        {
+            verts = new List<Vector3>();
+            norms = new List<Vector3>();
+
+            foreach(ModelMesh mesh in model.Meshes)
+            {
+                foreach(ModelMeshPart part in mesh.MeshParts)
+                {
+                    VertexPositionNormalTexture[] vertexData = new VertexPositionNormalTexture[part.NumVertices];
+
+                    ushort[] indices = new ushort[part.IndexBuffer.IndexCount];
+                    part.IndexBuffer.GetData<ushort>(indices);
+
+                    Vector3 v = new Vector3();
+                    for (int i =0; i<indices.Length; i++)
+                    {
+                        v.X = vertexData[indices[i]].Position.X;
+                        v.Y = vertexData[indices[i]].Position.Y;
+                        v.Z = vertexData[indices[i]].Position.Z;
+
+                        verts.Add(Vector3.Transform(v, mesh.ParentBone.Transform * world));
+
+                        if(verts.Count % 3 == 0)
+                        {
+                            Vector3 Normal = Vector3.Cross(verts[verts.Count - 1]- verts[verts.Count-3], 
+                                                            verts[verts.Count-2]- verts[verts.Count - 3]);
+                            Normal.Normalize();
+                            norms.Add(Normal);
+                        }
+                    }
+                }
+            }
+        }
+
+        public static bool SameSide(Vector3 p1, Vector3 p2, Vector3 a, Vector3 b)
+        {
+            Vector3 cp1 = Vector3.Cross(b - a, p1 - a);
+            Vector3 cp2 = Vector3.Cross(b - a, p2 - a);
+            if(Vector3.Dot(cp1, cp2)>= 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool PointInTrangle(Vector3 p, Vector3 a, Vector3 b, Vector3 c)
+        {
+            if(SameSide(p, a, b, c) && SameSide(p, b, a, c) && SameSide(p, a, c, b))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool checkCollisionAndResponse(ball e)
+        {
+            bool collision = false;
+
+            //need his notes here 
+
+
+
+            return collision;
+
+        }
+    }
+}
--- Game1.cs
+++ Game1.cs
@@ -12,9 +12,11 @@ namespace bounce_Ball
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
-        Model ball, plane;
+        Model ball, plane, dish;
         Matrix world, view, proj;
         ball b;
+
+        MeshCollider worldmesh;//fix
 
         List<ball> ballList;
 
@@ -24,8 +26,9 @@ namespace bounce_Ball
         //        ballList.Add(b = new bounce_ball.ball());
         //    }
 
-    Random rand;
+        Random rand;
         float fov;
+        float camRotY;
 
         public Game1()
         {
@@ -39,18 +42,22 @@ namespace bounce_Ball
             // TODO: Add your initialization logic here
 
             fov = 60;
+            camRotY = 0;
+
+
 
   
             base.Initialize();
         }
 
-        protected override void LoadContent()
+        protected override void LoadContent()//load in mesh
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
             ball = Content.Load<Model>("sphere");
             plane = Content.Load<Model>("plane");
+            dish = Content.Load<Model>("dish");
 
             rand = new Random();
             b = new ball(new Vector3(30, 25, 30),
@@ -82,12 +89,22 @@ namespace bounce_Ball
                 fov += .5f;
             }
 
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                camRotY -= .01f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                camRotY += .01f;
+            }
+
             // TODO: Add your update logic here
 
             foreach (ball b in ballList)
             {
                 b.Update(gameTime);
-                b.checkCollision(new Plane(Vector3.Up, 0f));
+                //b.checkCollision(new Plane(Vector3.Up, 0f));
+                worldmesh//need rest of code
 
                 // Check collision between balls 
                
@@ -106,7 +123,8 @@ namespace bounce_Ball
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
-            view = Matrix.CreateLookAt(new Vector3(50, 50, 50),
+            view = Matrix.CreateLookAt(//new Vector3(50, 50, 50),
+                                        Vector3.Transform(new Vector3(50,50,50), Matrix.CreateRotationY(camRotY)),
                                         Vector3.Zero,
                                         Vector3.Up);
             proj = Matrix.CreatePerspectiveFieldOfView(
@@ -114,8 +132,22 @@ namespace bounce_Ball
                             _graphics.PreferredBackBufferWidth/_graphics.PreferredBackBufferHeight,
                             1.0f,
                             1000f);
-            world = Matrix.CreateScale(100);
-            plane.Draw(world, view, proj);
+            //fpr plane
+            //world = Matrix.CreateScale(100);
+            //plane.Draw(world, view, proj);
+
+            world = Matrix.CreateScale(4);
+            foreach(ModelMesh mesh in dish.Meshes)
+            {
+                foreach(BasicEffect effect in mesh.Effects)
+                {
+                    effect.World = world;
+                    effect.View = view;
+                    effect.Projection = proj;
+                    effect.EnableDefaultLighting();
+                }
+                mesh.Draw();
+            }
 
             //world = Matrix.CreateTranslation(b.Pos);
 
